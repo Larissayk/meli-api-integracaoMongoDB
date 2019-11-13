109563d86d294d0fcbe80e8d6b45b5b545ab9923
Larissayk <lari.yukie@gmail.com>
2019-11-12 22:11:56 -0300

    get & get nasceuSP

--- src/controllers/professorasController.js
+++ src/controllers/professorasController.js
@@ -1,5 +1,5 @@
-const professoras = require("../model/professoras.json")
-const fs = require('fs');
+const professoras = require("../model/professoras.json");
+const fs = require("fs");
 
 // exports.get = (req, res) => {
 //   const arrProfs = []
@@ -16,36 +16,40 @@ const fs = require('fs');
 
 exports.get = (req, res) => {
   const profSemCpf = professoras.map(item => {
-     item.cpf = "*********"
-     return item
-  })
+    item.cpf = "*********";
+    return item;
+  });
 
   /* const profSemCpf = professoras.map(item => {
     delete item.cpf
     return item
   }) */
 
-  res.status(200).send(profSemCpf)
-}
+  res.status(200).send(profSemCpf);
+};
 
 exports.getById = (req, res) => {
-  const id = req.params.id
-  const prof = professoras.find(prof => prof.id == id)
-  delete prof.cpf
-  res.status(200).send(prof)
-}
+  const id = req.params.id;
+  const prof = professoras.find(prof => prof.id == id);
+  delete prof.cpf;
+  res.status(200).send(prof);
+};
 
-exports.post = (req, res) => { 
+exports.post = (req, res) => {
   const { id, nome, especialidade, signo, cpf } = req.body;
   professoras.push({ id, nome, especialidade, signo, cpf });
 
-  fs.writeFile("./src/model/professoras.json", JSON.stringify(professoras), 'utf8', function (err) {
-    if (err) {
-      return res.status(500).send({ message: err });
+  fs.writeFile(
+    "./src/model/professoras.json",
+    JSON.stringify(professoras),
+    "utf8",
+    function(err) {
+      if (err) {
+        return res.status(500).send({ message: err });
+      }
+      console.log("The file was saved!");
     }
-    console.log("The file was saved!");
-  }); 
+  );
 
   return res.status(201).send(professoras);
-}
-
+};
--- src/model/alunas.js
+++ src/model/alunas.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const AlunasSchema = new mongoose.Schema({
-    nome:{type: String, required: true},
-    dateOfBirth:{type: Date},
-    nasceuEmSp:{type: Boolean},
-    livros: [{
-        titulo:String,
-        leu:Boolean
-    }]
+const alunasSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  dateOfBirth: { type: Date },
+  nasceuEmSp: { type: Boolean },
+  livros: [
+    {
+      titulo: String,
+      leu: Boolean
+    }
+  ]
+});
 
-})
+const Alunas = mongoose.model("Alunas", alunasSchema);  //indicando para o mongoose que o Schema está relacionado a model "alunas"
+
+module.exports = Alunas;
